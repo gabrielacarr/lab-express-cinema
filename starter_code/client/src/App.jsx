aaaa379ef2ed872568c0f95e73f6ae84ae58a163
gabrielacarr <briecardo@icloud.com>
2020-05-14 17:10:17 -0400

    app

--- starter_code/client/src/App.jsx
+++ starter_code/client/src/App.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
 import Home from "./components/Home";
+import { Switch, Route } from "react-router-dom";
+import Movies from "./components/Movies";
+import Movie from "./components/Movie";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home />
+        <Switch>
+          <Route exact path="/" render={(props) => <Home {...props} />} />
+          <Route
+            exact
+            path="/movies"
+            render={(props) => <Movies {...props} />}
+          />
+          <Route
+            exact
+            path="/movies/:id"
+            render={(props) => <Movie {...props} />}
+          />
+        </Switch>
       </div>
     );
   }
 }
-
 export default App;
